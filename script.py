a5b2c3961a2b590821653c4fe19f48cb0a156219
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2024-01-03 11:28:50 +0200

    Hard-code ports and update message since the args have now been removed from oobabooga

--- script.py
+++ script.py
@@ -7,9 +7,9 @@ from modules.logging_colors import logger
 
 
 def setup():
-    logger.warning("\nThe current API is deprecated and will be replaced with the OpenAI compatible API on November 13th.\nTo test the new API, use \"--extensions openai\" instead of \"--api\".\nFor documentation on the new API, consult:\nhttps://github.com/oobabooga/text-generation-webui/wiki/12-%E2%80%90-OpenAI-API")
-    blocking_api.start_server(shared.args.api_blocking_port, share=shared.args.public_api, tunnel_id=shared.args.public_api_id)
+    logger.warning("\nThe legacy API has been replaced with the OpenAI compatible API on November 13th.\nPlease switch to the new API as soon as possible.\nFor documentation on the new API, consult:\nhttps://github.com/oobabooga/text-generation-webui/wiki/12-%E2%80%90-OpenAI-API")
+    blocking_api.start_server(6050, share=shared.args.public_api, tunnel_id=shared.args.public_api_id)
     if shared.args.public_api:
         time.sleep(5)
 
-    streaming_api.start_server(shared.args.api_streaming_port, share=shared.args.public_api, tunnel_id=shared.args.public_api_id)
+    streaming_api.start_server(6055, share=shared.args.public_api, tunnel_id=shared.args.public_api_id)
